915c767a710b1dec5b117ee59a361c0c5e1881c8
Amr Hassan <amr.hassan799@gmail.com>
2021-05-03 00:17:20 +0200

    KeyPad Driver Updated

--- KeyPad.h
+++ KeyPad.h
@@ -11,17 +11,16 @@
 
 #include "DIO_CFG.h"
 
-#define KeyPad_DDR_Reg DDRD_reg
-#define KeyPad_PortReg PORTD_reg
+#define KeyPad_DDR_Reg DDRC_reg
+#define KeyPad_PortReg PORTC_reg
 
-#define C1 DIO_ChannelD0
-#define C2 DIO_ChannelD1
-#define C3 DIO_ChannelD2
-#define C4 DIO_ChannelD3
-#define R1 DIO_ChannelD4
-#define R2 DIO_ChannelD5
-#define R3 DIO_ChannelD6
-#define R4 DIO_ChannelD7
+#define R1 DIO_ChannelC0
+#define R2 DIO_ChannelC1
+#define R3 DIO_ChannelC2
+#define R4 DIO_ChannelC3
+#define C1 DIO_ChannelC4
+#define C2 DIO_ChannelC5
+#define C3 DIO_ChannelC6
 
 
 void KeyPad_Init(void);
